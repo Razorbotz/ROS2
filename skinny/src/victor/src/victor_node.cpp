91b2bd928386704ee7547b8dddb415ee5edd69cd
Luke Simmons <luke5083@live.com>
2021-10-30 12:58:33 -0500

    Upgrade Victor to ROS2
    Now compiles correctly.

--- skinny/src/victor/src/victor_node.cpp
+++ skinny/src/victor/src/victor_node.cpp
@@ -21,47 +21,46 @@ using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
 
 bool GO = false;
-void stopCallback(std_msgs::Empty empty) {
+void stopCallback(std_msgs::msg::Empty::SharedPtr) {
 	std::cout << "STOP" << std::endl;
 	GO = false;
 }
-void goCallback(std_msgs::Empty empty) {
+void goCallback(std_msgs::msg::Empty::SharedPtr) {
 	std::cout << "GO" << std::endl;
 	GO = true;
 }
 
 VictorSPX* victorSPX;
-void speedCallback(const std_msgs::Float32 speed) {
-	std::cout << speed.data << std::endl;
-	victorSPX->Set(ControlMode::PercentOutput, speed.data);
+void speedCallback(const std_msgs::msg::Float32::SharedPtr speed) {
+	std::cout << speed->data << std::endl;
+	victorSPX->Set(ControlMode::PercentOutput, speed->data);
 }
 
 int main(int argc, char** argv) {
-	std::cout << "Starting talon 3" << std::endl;
-	ros::init(argc, argv, "talon_node");
-	ros::NodeHandle nodeHandleP("~");
-	ros::NodeHandle nodeHandle;
+	std::cout << "Starting victor" << std::endl;
+	rclcpp::init(argc, argv);
+	auto nodeHandle = rclcpp::Node::make_shared("victor");
 
 	int success;
 
 	std::string message;
-	success = nodeHandleP.getParam("message", message);
+	success = nodeHandle->get_parameter("message", message);
 	std::cout << success << "message " << message << std::endl;
 
 	int motorNumber = 0;
-	success = nodeHandleP.getParam("motor_number", motorNumber);
+	success = nodeHandle->get_parameter("motor_number", motorNumber);
 	std::cout << success << "motor_number: " << motorNumber << std::endl;
 
 	std::string infoTopic;
-	success = nodeHandleP.getParam("info_topic", infoTopic);
+	success = nodeHandle->get_parameter("info_topic", infoTopic);
 	std::cout << success << "info_topic: " << infoTopic << std::endl;
 
 	std::string speedTopic;
-	success = nodeHandleP.getParam("speed_topic", speedTopic);
+	success = nodeHandle->get_parameter("speed_topic", speedTopic);
 	std::cout << success << "speed_topic: " << speedTopic << std::endl;
 
 	bool invertMotor = false;
-	success = nodeHandleP.getParam("invert_motor", invertMotor);
+	success = nodeHandle->get_parameter("invert_motor", invertMotor);
 	std::cout << success << "invert_motor: " << invertMotor << std::endl;
 
 	ctre::phoenix::platform::can::SetCANInterface("can0");
@@ -69,36 +68,36 @@ int main(int argc, char** argv) {
 	victorSPX = new VictorSPX(motorNumber);
 	victorSPX->SetInverted(invertMotor);
 	victorSPX->Set(ControlMode::PercentOutput, 0);
-	StatusFrame statusFrame;
+	//StatusFrame statusFrame;
 
-	messages::VictorOut victorOut;
-	ros::Publisher victorOutPublisher = nodeHandle.advertise<messages::VictorOut>(infoTopic.c_str(), 1);
-	ros::Subscriber speedSubscriber = nodeHandle.subscribe(speedTopic.c_str(), 1, speedCallback);
+	messages::msg::VictorOut victorOut;
+	auto victorOutPublisher = nodeHandle->create_publisher<messages::msg::VictorOut>(infoTopic.c_str(), 1);
+	auto speedSubscriber = nodeHandle->create_subscription<std_msgs::msg::Float32>(speedTopic.c_str(), 1, speedCallback);
 
-	ros::Subscriber stopSubscriber = nodeHandle.subscribe("STOP", 1, stopCallback);
-	ros::Subscriber goSubscriber = nodeHandle.subscribe("GO", 1, goCallback);
+	auto stopSubscriber = nodeHandle->create_subscription<std_msgs::msg::Empty>("STOP", 1, stopCallback);
+	auto goSubscriber = nodeHandle->create_subscription<std_msgs::msg::Empty>("GO", 1, goCallback);
 	auto start = std::chrono::high_resolution_clock::now();
-	while(ros::ok()) {
+	while(rclcpp::ok()) {
 		if(GO) ctre::phoenix::unmanaged::FeedEnable(100);
 
 		auto finish = std::chrono::high_resolution_clock::now();
 		if(std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() > 250000000) {
 			int deviceID = victorSPX->GetDeviceID();
 			double busVoltage = victorSPX->GetBusVoltage();
-			bool isInverted = victorSPX->GetInverted();
+			//bool isInverted = victorSPX->GetInverted();
 			double motorOutputVoltage = victorSPX->GetMotorOutputVoltage();
 			double motorOutputPercent = victorSPX->GetMotorOutputPercent();
 
-			victorOut.deviceID = deviceID;
-			victorOut.busVoltage = busVoltage;
-			victorOut.outputVoltage = motorOutputVoltage;
-			victorOut.outputPercent = motorOutputPercent;
+			victorOut.device_id = deviceID;
+			victorOut.bus_voltage = busVoltage;
+			victorOut.output_voltage = motorOutputVoltage;
+			victorOut.output_percent = motorOutputPercent;
 
-			victorOutPublisher.publish(victorOut);
+			victorOutPublisher->publish(victorOut);
 			start = std::chrono::high_resolution_clock::now();
 		}
 
 		usleep(20);
-		ros::spinOnce();
+		rclcpp::spin_some(nodeHandle);
 	}
 }
