6aa23778dcbb68720b8125634e3d64be73b6cc77
Andrew Burroughs <andrewburroughs17@gmail.com>
2021-12-02 12:35:53 -0600

    Created MainPage.cpp for main documentation page and fixed .msg file documentation

---
+++ MainPage.cpp
@@ -0,0 +1,50 @@
+/*!
+*  \mainpage Razorbotz RMC 2021-2022 Documentation Home Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \file skinny/src/messages/msg/AxisState.msg
+ * AxisState.msg
+ * \verbinclude skinny/src/messages/msg/AxisState.msg
+ * 
+ * \file ButtonState.msg
+ * ButtonState.msg
+ * \verbinclude ButtonState.msg
+ * 
+ * \file Camera.msg
+ * Camera.msg
+ * \verbinclude Camera.msg
+ * 
+ * \file HatState.msg
+ * HatState.msg
+ * \verbinclude HatState.msg
+ * 
+ * \file JoystickCommand.msg
+ * JoystickCommand.msg
+ * \verbinclude JoystickCommand.msg
+ * 
+ * \file KeyState.msg
+ * KeyState.msg
+ * \verbinclude KeyState.msg
+ * 
+ * \file Power.msg
+ * Power.msg
+ * \verbinclude Power.msg
+ * 
+ * 
+ * \file TalonOut.msg
+ * TalonOut.msg
+ * \verbinclude TalonOut.msg
+ * 
+ * \file VictorOut.msg
+ *  VictorOut.msg
+ *  \verbinclude VictorOut.msg
+ * 
+ * \file ZedPosition.msg
+ * ZedPosition.msg
+ * \verbinclude ZedPosition.msg
+ */
