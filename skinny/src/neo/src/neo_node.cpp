80cf48baf376a5d2d81d500d52e23719d57c9971
andrewburroughs <andrewburroughs17@gmail.com>
2022-03-15 01:28:36 -0500

    Created Neo and Linear actuator nodes, Updated Talon node

--- skinny/src/neo/src/neo_node.cpp
+++ skinny/src/neo/src/neo_node.cpp
@@ -28,26 +28,14 @@
 #include <std_msgs/msg/float32_multi_array.hpp>
 #include <std_msgs/msg/empty.hpp>
 
-#define Phoenix_No_WPI // remove WPI dependencies
-#include <ctre/Phoenix.h>
-#include <ctre/phoenix/platform/Platform.h>
-#include <ctre/phoenix/unmanaged/Unmanaged.h>
-#include <ctre/phoenix/cci/Unmanaged_CCI.h>
-#include <ctre/phoenix/cci/Diagnostics_CCI.h>
-
-#include "messages/msg/talon_out.hpp"
-
-using namespace ctre::phoenix;
-using namespace ctre::phoenix::platform;
-using namespace ctre::phoenix::motorcontrol;
-using namespace ctre::phoenix::motorcontrol::can;
+#include <rev/CANSparkMax.h>
 
 /** @file
- * @brief Node controlling one Talon motor 
+ * @brief Node controlling one Neo motor 
  * 
  * This node receives information published by the logic node,
  * then transforms the data received into movement by the motor
- * controlled by the Talon instance.  The topics that the node
+ * controlled by the Neo instance.  The topics that the node
  * subscribes to are as follows:
  * \li \b speed_topic
  * \li \b STOP
@@ -63,13 +51,12 @@ using namespace ctre::phoenix::motorcontrol::can;
  * The topics being published are as follows:
  * \li \b info_topic
  * 
- * This string has the general form talon_{motorNumber}_info and
+ * This string has the general form neo_{motorNumber}_info and
  * is defined by the user in the launch file.  To read more about
  * the launch file,
  * \see launch.py
  * 
  * */
-
 
 rclcpp::Node::SharedPtr nodeHandle;
 //bool GO=false;
@@ -192,120 +179,28 @@ T getParameter(std::string parameterName, int initialValue, int type){
 	return value;
 }
 
-int main(int argc,char** argv){
+int main(int argc, char** argv){
 	rclcpp::init(argc,argv);
-	nodeHandle = rclcpp::Node::make_shared("talon");
-
-	RCLCPP_INFO(nodeHandle->get_logger(),"Starting talon");
-
+	nodeHandle = rclcpp::Node::make_shared("neo");
+	RCLCPP_INFO(nodeHandle->get_logger(),"Starting neo");
 	int motorNumber = getParameter<int>("motor_number", 1, 0);
-	int portNumber = getParameter<int>("diagnostics_port", 1, 0);	
-	c_Phoenix_Diagnostics_Create1(portNumber);
 	std::string infoTopic = getParameter<std::string>("info_topic", "unset");
 	std::string speedTopic = getParameter<std::string>("speed_topic", "unset");
 	bool invertMotor = getParameter<bool>("invert_motor", 0, 1);
 	useVelocity = getParameter<bool>("use_velocity", 0, 1);
 	velocityMultiplier = getParameter<int>("velocity_multiplier", 0, 0);
 	testSpeed = getParameter<int>("test_speed", 0, 0);
-	double kP = getParameter<double>("kP", 1, 2);
-	double kI = getParameter<double>("kI", 0, 2);
-	double kD = getParameter<double>("kD", 0, 2);
-	double kF = getParameter<double>("kF", 0, 2);
 
-	ctre::phoenix::platform::can::SetCANInterface("can0");
-	RCLCPP_INFO(nodeHandle->get_logger(),"Opened CAN interface");
+	rev::CANSparkMax sparkMax{motorNumber, rev::CANSparkMax::MotorType::kBrushless};
+	sparkMax.RestoreFactoryDefaults();
 
-	int kTimeoutMs=30;
-	int kPIDLoopIdx=0;
-	//int kSlotIdx=0;
-	talonSRX=new TalonSRX(motorNumber);
-	RCLCPP_INFO(nodeHandle->get_logger(),"created talon instance");
-
-	talonSRX->SetInverted(invertMotor);
-	RCLCPP_INFO(nodeHandle->get_logger(),"here 1");
-
-	talonSRX->SelectProfileSlot(0,0);
-	talonSRX->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, kTimeoutMs);
-	//talonSRX->SetSensorPhase(false);
-	talonSRX->ConfigClosedloopRamp(2);
-	talonSRX->ConfigNominalOutputForward(0, kTimeoutMs);
-	talonSRX->ConfigNominalOutputReverse(0, kTimeoutMs);
-	talonSRX->ConfigPeakOutputForward(1, kTimeoutMs);
-	talonSRX->ConfigPeakOutputReverse(-1, kTimeoutMs);
-	talonSRX->Config_kF(kPIDLoopIdx, kF, kTimeoutMs);
-	talonSRX->Config_kP(kPIDLoopIdx, kP, kTimeoutMs);
-	talonSRX->Config_kI(kPIDLoopIdx, kI, kTimeoutMs);
-	talonSRX->Config_kD(kPIDLoopIdx, kD, kTimeoutMs);
-	talonSRX->ConfigAllowableClosedloopError(kPIDLoopIdx,0,kTimeoutMs);
-
-	talonSRX->Set(ControlMode::PercentOutput, 0);
-	talonSRX->Set(ControlMode::Velocity, 0);
-
-	RCLCPP_INFO(nodeHandle->get_logger(),"configured talon");
-
-	//TalonSRXPIDSetConfiguration pid;
-	TalonSRXConfiguration allConfigs;
-	//StatusFrame statusFrame;
-
-	messages::msg::TalonOut talonOut;
-	//ros::Publisher talonOutPublisher=nodeHandle.advertise<messages::TalonOut>(infoTopic.c_str(),1);
-	auto talonOutPublisher=nodeHandle->create_publisher<messages::msg::TalonOut>(infoTopic.c_str(),1);
-	//ros::Subscriber speedSubscriber=nodeHandle.subscribe(speedTopic.c_str(),1,speedCallback);
-	auto speedSubscriber=nodeHandle->create_subscription<std_msgs::msg::Float32>(speedTopic.c_str(),1,speedCallback);
-
-	//ros::Subscriber stopSubscriber=nodeHandle.subscribe("STOP",1,stopCallback); 
+	auto speedSubscriber=nodeHandle->create_subscription<std_msgs::msg::Float32(speedTopic.c_str(),1,speedCallback);
 	auto stopSubscriber=nodeHandle->create_subscription<std_msgs::msg::Empty>("STOP",1,stopCallback);
-	//ros::Subscriber goSubscriber=nodeHandle.subscribe("GO",1,goCallback); 
-	auto goSubscriber=nodeHandle->create_subscription<std_msgs::msg::Empty>("GO",1,goCallback);
-	RCLCPP_INFO(nodeHandle->get_logger(),"set subscribers");
+	auto goSubscriber=nodeHandle->create_subscription<std_msgs::msg::Empty("GO", 1, goCallback);
+	RCLCPP_INFO(nodeHandle->get_logger(), "Set subscribers");
 
-	rclcpp::Rate rate(20);
-	int count=0;
-	auto start = std::chrono::high_resolution_clock::now();
+	RCLCPP_INFO(nodeHandle->get_logger(),"Created NEO");
 	while(rclcpp::ok()){
-		if(GO)ctre::phoenix::unmanaged::FeedEnable(100);
-		auto finish = std::chrono::high_resolution_clock::now();
-
-		if(std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() > 250000000){
-			int deviceID=talonSRX->GetDeviceID();
-			double busVoltage=talonSRX->GetBusVoltage();
-			double outputCurrent=talonSRX->GetOutputCurrent();
-			bool isInverted=talonSRX->GetInverted();
-			double motorOutputVoltage=talonSRX->GetMotorOutputVoltage();
-			double motorOutputPercent=talonSRX->GetMotorOutputPercent();
-			double temperature=talonSRX->GetTemperature();
-			int sensorPosition0=talonSRX->GetSelectedSensorPosition(0);
-			int sensorVelocity0=talonSRX->GetSelectedSensorVelocity(0);
-			int closedLoopError0=talonSRX->GetClosedLoopError(0);
-			double integralAccumulator0=talonSRX->GetIntegralAccumulator(0);
-			double errorDerivative0=talonSRX->GetErrorDerivative(0);
-		
-			talonOut.device_id=deviceID;	
-			talonOut.bus_voltage=busVoltage;
-			talonOut.output_current=outputCurrent;
-			talonOut.output_voltage=motorOutputVoltage;
-			talonOut.output_percent=motorOutputPercent;
-			talonOut.temperature=temperature;
-			talonOut.sensor_position=sensorPosition0;
-			talonOut.sensor_velocity=sensorVelocity0;
-			talonOut.closed_loop_error=closedLoopError0;
-			talonOut.integral_accumulator=integralAccumulator0;
-			talonOut.error_derivative=errorDerivative0;
-
-			talonOutPublisher->publish(talonOut);
-        		start = std::chrono::high_resolution_clock::now();
-		}
-
-		if(count++>200 && GO){
-			std::cout <<"V=" << talonSRX->GetSelectedSensorVelocity(kPIDLoopIdx) <<"  "
-				<< "  E=" << talonSRX->GetClosedLoopError(kPIDLoopIdx) 
-				<< "  IA=" << talonSRX->GetIntegralAccumulator(kPIDLoopIdx)
-				<< "  ED=" << talonSRX->GetErrorDerivative(kPIDLoopIdx) 
-				<< std::endl;
-			count=0;
-		}
-		rate.sleep();
 		rclcpp::spin_some(nodeHandle);
 	}
 }
-
