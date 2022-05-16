e332c53d921037af675fd673ea597fb3abbf5e5a
andrewburroughs <andrewburroughs17@gmail.com>
2022-05-15 20:35:30 -0500

    Modified code for Spinner

--- spinner/src/excavation/excavation/excavation_node.py
+++ spinner/src/excavation/excavation/excavation_node.py
@@ -5,13 +5,19 @@ from odrive.enums import *
 import time
 from std_msgs.msg import Float32
 from std_msgs.msg import String
+from std_msgs.msg import Empty
 import fibre.utils
 
+# ToDo:
+# error handling
+
+
 class ExcavationNode(Node):
-	def __init__(self):
+        def __init__(self):
                 super().__init__('excavation_node')
                 self.get_logger().info("odriveObjects")
                 self.calibrated = False
+                self.GO = False
                 self.findODriveObjects()
                 self.get_logger().info("Found ODriveObjects")
                 self.calibrate()
@@ -20,67 +26,135 @@ class ExcavationNode(Node):
                 self.excavationDrumSubscription = self.create_subscription(Float32, 'excavationDrum', self.excavationDrumCallback, 10)
                 self.get_logger().info("excavationArmSubscription")
                 self.excavationArmSubscription = self.create_subscription(Float32, 'excavationArm', self.excavationArmCallback, 10)
+                self.stopSubscription = self.create_subscription(Empty, 'STOP', self.stopCallback, 1)
+                self.goSubscription = self.create_subscription(Empty, 'GO', self.goCallback, 1)
                 self.get_logger().info('HERE')
 
-	def findODriveObjects(self):
-		self.get_logger().info("findOdriveObjects start")
-		self.odrv1 = odrive.find_any(path = "usb", serial_number = "20773881304E")
-		self.odrv0 = odrive.find_any(path = "usb", serial_number = "2083367F424D")
+        def errorChecking(self):
+                if self.odrv0.error != 0:
+                        self.get_logger().info("ERROR: odrv0 has error state: ", self.odrv0.error)
+                        self.calibrate0()
+                if self.odrv1.error != 0:
+                        self.get_logger().info("ERROR: odrv1 has error state: ", self.odrv1.error)
+                        self.calibrate1()
 
-	##################################################################################
-	#Note: This setup relies on the motors being in the following positions:         #
-	#The first three motors are position controlled.  The motors of odrv0 are assumed#
-	#to be the motors that control elevation of the assembly and are mirrored.       #
-	#The first motor of odrv1 is assumed to control the angle of the assembly and is #
-	#also position controlled.  The final motor is assumed to control the drill and  #
-	#is currently configured to run sensorlessly because we do not have the final    #
-	#encoder needed to run it with sensors.						 #
-	##################################################################################
-	def setRequestedState(self):
-		self.get_logger().info("setRequestedState start")
-		self.odrv0.axis1.controller.config.axis_to_mirror = 0
-		self.odrv0.axis1.controller.config.input_mode = 7
-		self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-		self.odrv0.axis0.controller.config.control_mode = 2
-		self.odrv0.axis0.controller.input_vel = 0
-		self.odrv1.axis1.controller.config.axis_to_mirror = 0
-		self.odrv1.axis1.controller.config.input_mode = 7
-		self.odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-		self.odrv1.axis0.controller.config.control_mode = 3
-		self.odrv1.axis0.controller.input_pos = 0
-		self.calibrated = True
+        def calibrate0(self):
+                self.get_logger().info("Calibrating odrv0 from error state")
+                self.odrv0.clear_errors()
+                self.odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv0.axis0.current_state != AXIS_STATE_IDLE:
+	                time.sleep(0.1)
+                self.odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv0.axis1.current_state != AXIS_STATE_IDLE:
+                        time.sleep(0.1)
+                self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-	def excavationDrumCallback(self, msg):
-		if self.calibrated:
-			self.odrv0.axis0.controller.input_vel = msg.data * 20
-			self.get_logger().info("excavationDrumCallback: msg.data: " + str(msg.data))
-			self.get_logger().info("input_vel: " + str(self.odrv0.axis0.controller.input_vel))
+        def calibrate1(self):
+                self.get_logger().info("Calibrating odrv1 from error state")
+                self.odrv1.clear_errors()
+                self.odrv1.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv1.axis0.current_state != AXIS_STATE_IDLE:
+                        time.sleep(0.1)
+                self.odrv1.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv1.axis1.current_state != AXIS_STATE_IDLE:
+                        time.sleep(0.1)
+                self.get_logger().info("Calibrated odrv1 from error state.  Current error state: ", self.odrv1.error)
+                self.odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-	def excavationArmCallback(self, msg):
-		if self.calibrated:
-			self.odrv1.axis0.controller.input_pos += msg.data
-			self.get_logger().info("excavationArmCallback: msg.data: " + str(msg.data))
-			self.get_logger().info("input_pos: " + str(self.odrv1.axis0.controller.input_pos))
+        # each odrv object contains two motors
+        def findODriveObjects(self):
+                self.get_logger().info("findOdriveObjects start")
+                # odrv0 = drum
+                self.odrv0 = odrive.find_any(path = "usb", serial_number = "2083367F424D")
+                # odrv1 = arm
+                self.odrv1 = odrive.find_any(path = "usb", serial_number = "20773881304E")
+                # self.odrv0 = odrive.find_any(path = "usb", serial_number = "2083367F424D")
 
-	def calibrate(self):
-		self.get_logger().info("calibrate start")
-#		self.odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-		self.odrv1.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-		while self.odrv0.axis0.current_state != AXIS_STATE_IDLE | self.odrv1.axis0.current_state != AXIS_STATE_IDLE:
-			time.sleep(0.1)
-#		self.odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-		self.odrv1.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-		while self.odrv0.axis1.current_state != AXIS_STATE_IDLE | self.odrv1.axis1.current_state != AXIS_STATE_IDLE:
-			time.sleep(0.1)
-		self.get_logger().info("Calibrated")
+        ##################################################################################
+        #Note: This setup relies on the motors being in the following positions:         #
+        #The first three motors are position controlled.  The motors of odrv0 are assumed#
+        #to be the motors that control elevation of the assembly and are mirrored.       #
+        #The first motor of odrv1 is assumed to control the angle of the assembly and is #
+        #also position controlled.  The final motor is assumed to control the drill and  #
+        #is currently configured to run sensorlessly because we do not have the final    #
+        #encoder needed to run it with sensors.						 #
+        ##################################################################################
+        def setRequestedState(self):
+                self.get_logger().info("setRequestedState start")
+                #self.odrv0.axis1.controller.config.axis_to_mirror = 0
+                #self.odrv0.axis1.controller.config.input_mode = 7
+                #self.odrv0.axis0.controller.config.control_mode = 2
+                #self.odrv0.axis0.controller.input_vel = 0
+                #self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+                self.odrv1.axis1.controller.config.axis_to_mirror = 0
+                self.odrv1.axis1.controller.config.input_mode = 7
+                self.odrv1.axis0.controller.config.control_mode = 2
+                self.odrv1.axis0.controller.input_vel = 0
+                self.odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+                self.odrv0.axis1.controller.config.control_mode = 2
+                self.odrv0.axis1.controller.input_vel = 0
+                self.odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+                self.calibrated = True
+
+        def excavationDrumCallback(self, msg):
+                if self.calibrated and self.GO:
+                        newSpeed = msg.data * 144
+                        oldSpeed = self.odrv0.axis1.controller.input_vel
+                        if abs(newSpeed - oldSpeed) > 5:
+                                if newSpeed > oldSpeed:
+                                        self.odrv0.axis1.controller.input_vel = oldSpeed + 5
+                                else:
+                                        self.odrv0.axis1.controller.input_vel = oldSpeed - 5
+                        else:
+                                self.odrv0.axis1.controller.input_vel = msg.data * 144
+                        self.get_logger().info("excavationDrumCallback: msg.data: " + str(msg.data))
+                        self.get_logger().info("input_vel: " + str(self.odrv0.axis1.controller.input_vel))
+
+        def excavationArmCallback(self, msg):
+                if self.calibrated and self.GO:
+                        newSpeed = msg.data * 20
+                        oldSpeed = self.odrv1.axis1.controller.input_vel
+                        if abs(newSpeed - oldSpeed) > 2:
+                                if newSpeed > oldSpeed:
+                                        self.odrv1.axis0.controller.input_vel = oldSpeed + 2
+                                else:
+                                        self.odrv1.axis0.controller.input_vel = oldSpeed - 2
+                        else:
+                                self.odrv1.axis0.controller.input_vel = msg.data * 20
+                        self.get_logger().info("excavationArmCallback: msg.data: " + str(msg.data))
+                        self.get_logger().info("input_vel: " + str(self.odrv1.axis0.controller.input_vel))
+
+        def stopCallback(self, msg):
+                self.GO = False
+                self.odrv0.axis0.controller.input_vel = 0
+                self.odrv1.axis1.controller.input_vel = 0
+
+        def goCallback(self, msg):
+                self.GO = True
+
+        def calibrate(self):
+                self.get_logger().info("calibrate start")
+#                self.odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+#                while self.odrv0.axis0.current_state != AXIS_STATE_IDLE:
+#                        time.sleep(0.1)
+                self.odrv1.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv1.axis0.current_state != AXIS_STATE_IDLE:
+                        time.sleep(0.1)
+                self.odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv1.axis0.current_state != AXIS_STATE_IDLE:
+                        time.sleep(0.1)
+                self.odrv1.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while self.odrv1.axis1.current_state != AXIS_STATE_IDLE:
+                        time.sleep(0.1)
+                self.get_logger().info("Calibrated")
 
 def main(args=None):
-	rclpy.init(args=args)
-	excavation_node = ExcavationNode()
-	rclpy.spin(excavation_node)
+        rclpy.init(args=args)
+        excavation_node = ExcavationNode()
+        rclpy.spin(excavation_node)
 
-	excavation_node.destroy_node()
-	rclpy.shutdown()
+        excavation_node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
