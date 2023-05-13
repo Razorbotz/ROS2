cfda57bdb31d1a11705717ca6bb25068225dd45f
andrewburroughs <andrewburroughs17@gmail.com>
2023-05-13 16:25:23 -0500

    Added NewLaunch with no talons

--- scoop/launch/NewLaunch.py
+++ scoop/launch/NewLaunch.py
@@ -8,14 +8,48 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='talon',
-#            namespace='',
-            name='front_left_motor',
-#            executable='talon_node',
-            node_executable='talon_node',
+            package='arduino',
+            name='arduino',
+            executable='arduino_node'
+        )
+        ,
+        Node(
+            package='logic',
+            name='logic',
+            executable='logic_node',
+            output={'stderr': 'screen', 'stdout': 'screen'}
+        )
+        ,
+        Node(
+            package='communication2',
+            name='communication2',
+            executable='communication2_node',
+            parameters=[
+                {"robot_name": "Scoop"}
+            ],
+            output={'stderr': 'screen', 'stdout': 'screen'}
+        )
+        ,
+        Node(
+            package='power_distribution_panel',
+            name='power_distribution_panel',
+            executable='power_distribution_panel_node',
+            output={'stderr': 'screen', 'stdout': 'screen'}
+        )
+        ,
+        Node(
+            package='excavation',
+            name='excavation',
+            executable='excavation_node'
+        )
+        ,
+        Node(
+            package='falcon',
+            name='Falcon10',
+            executable='falcon_node',
             parameters=[
                 {"motor_number": 10},
-                {"diagnostics_port": 56710},
+                {"diagnostics_port": 72340},
                 {"invert_motor": False},
                 {"speed_topic": "drive_right_speed"},
                 {"info_topic": "talon_10_info"},
@@ -28,16 +62,14 @@ def generate_launch_description():
                 {"kF": 0.0}
             ]
         )
-        ,
+	    ,
         Node(
-            package='talon',
-#            namespace='',
-            name='rear_left_motor',
-#            executable='talon_node',
-            node_executable='talon_node',
+            package='falcon',
+            name='Falcon11',
+            executable='falcon_node',
             parameters=[
                 {"motor_number": 11},
-                {"diagnostics_port": 56711},
+                {"diagnostics_port": 72341},
                 {"invert_motor": True},
                 {"speed_topic": "drive_left_speed"},
                 {"info_topic": "talon_11_info"},
@@ -50,16 +82,14 @@ def generate_launch_description():
                 {"kF": 0.0}
             ]
         )
-        ,
+	    ,
         Node(
-            package='talon',
-#            namespace='',
-            name='front_right_motor',
-#            executable='talon_node',
-            node_executable='talon_node',
+            package='falcon',
+            name='Falcon12',
+            executable='falcon_node',
             parameters=[
                 {"motor_number": 12},
-                {"diagnostics_port": 56712},
+                {"diagnostics_port": 72342},
                 {"invert_motor": False},
                 {"speed_topic": "drive_right_speed"},
                 {"info_topic": "talon_12_info"},
@@ -74,14 +104,12 @@ def generate_launch_description():
         )
         ,
         Node(
-            package='talon',
-#            namespace='',
-            name='rear_right_motor',
-#            executable='talon_node',
-            node_executable='talon_node',
+            package='falcon',
+            name='Falcon13',
+            executable='falcon_node',
             parameters=[
                 {"motor_number": 13},
-                {"diagnostics_port": 56713},
+                {"diagnostics_port": 72343},
                 {"invert_motor": True},
                 {"speed_topic": "drive_left_speed"},
                 {"info_topic": "talon_13_info"},
@@ -96,50 +124,20 @@ def generate_launch_description():
         )
         ,
         Node(
-            package='logic',
-#            namespace='',
-            name='logic',
-#            executable='logic_node'
-            node_executable='logic_node'
-#            node_executable='logic_node',
-#            output={'stderr': 'screen', 'stdout': 'screen'}
+            package='zed_tracking',
+            name='zed_tracking',
+            executable='zed_tracking_node'
         )
         ,
         Node(
-            package='communication',
-#            namespace='',
-            name='communication',
-#            executable='communication_node',
-            node_executable='communication_node',
+            package='neo',
+            name='neo',
+            executable='neo_node',
             parameters=[
-                {"robot_name": "Skinny"}
+                {"motor_number": 18},
+                {"info_topic": "neoOut"},
+                {"speed_topic": "neo_speed"}
             ]
         )
-        ,
-        Node(
-            package='power_distribution_panel',
-#            namespace='',
-            name='power_distribution_panel',
-#            executable='power_distribution_panel_node'
-            node_executable='power_distribution_panel_node'
-        )
-#        ,
-#        Node(
-#            package='autonomy',
-#            name='autonomy',
-#            node_executable='autonomy_node'
-#        )
-#        ,
-#        Node(
-#            package='zed_tracking',
-#            name='zed_tracking',
-#            node_executable='zed_tracking_node'
-#        )
-#        ,
-#        Node(
-#            package='excavation',
-#            name='excavation',
-#            node_executable='excavation_node'
-#        )
     ]
 )
