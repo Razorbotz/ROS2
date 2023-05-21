97bf13b7a59c45d8e819a0e8b04f5672bee9983a
andrewburroughs <andrewburroughs17@gmail.com>
2023-05-21 18:45:01 -0500

    Fixed errors

--- scoop/src/stepper/stepper/stepper_node.py
+++ scoop/src/stepper/stepper/stepper_node.py
@@ -11,25 +11,33 @@ class StepperNode(Node):
         self.get_logger().info('stepper_node')
         self.servoStateSubscriber = self.create_subscription(Float32, 'stepper_speed', self.stepperCallback, 10)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+        #GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+        #GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
 
     
     def stepperCallback(self, msg):
         # Extend Stepper, 18 HIGH, 16 LOW
         if msg.data == 1:
-            GPIO.output(18, GPIO.HIGH)
-            GPIO.output(16, GPIO.LOW)
+            #GPIO.output(18, GPIO.HIGH)
+            GPIO.output(24, GPIO.HIGH)
+            #GPIO.output(16, GPIO.LOW)
+            GPIO.output(23, GPIO.LOW)
             self.get_logger().info('Stepper: HIGH')
         # Retract Stepper, 18 HIGH, 16 HIGH
         elif msg.data == -1:
-            GPIO.output(18, GPIO.HIGH)
-            GPIO.output(16, GPIO.HIGH)
+            #GPIO.output(18, GPIO.HIGH)
+            GPIO.output(24, GPIO.HIGH)
+            #GPIO.output(16, GPIO.HIGH)
+            GPIO.output(23, GPIO.HIGH)
             self.get_logger().info('Stepper: LOW')
         # Do nothing, 18 LOW, 16 LOW
         else:
-            GPIO.output(18, GPIO.LOW)
-            GPIO.output(16, GPIO.LOW)
+            #GPIO.output(18, GPIO.LOW)
+            GPIO.output(24, GPIO.LOW)
+            #GPIO.output(16, GPIO.LOW)
+            GPIO.output(23, GPIO.LOW)
 
     
 def main(args=None):
