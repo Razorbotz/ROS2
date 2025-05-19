73e97ff51f08db164ba7899c9938f32c49fea22f
Andrew Burroughs <andrewburroughs17@gmail.com>
2025-05-18 20:52:45 -0500

    Added reset to launch file

--- shovel/launch/launch.py
+++ shovel/launch/launch.py
@@ -41,6 +41,13 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(cam_launch_file)
         )
+        ,
+        Node(
+            package='reset',
+            executable='reset_node',
+            name='reset_node',
+            output='screen'
+        )
         #,
         #ExecuteProcess(
         #   cmd=['ros2', 'bag', 'record', '-a'],
