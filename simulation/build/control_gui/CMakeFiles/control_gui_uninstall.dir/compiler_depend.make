c67b196de612e44768cedcdde17a8f367b403ce8
HaM2703 <mahanhayden123@gmail.com>
2026-01-26 22:01:40 -0600

    Relocated files.

---
+++ simulation/build/control_gui/CMakeFiles/control_gui_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for control_gui_uninstall.
+# This may be replaced when dependencies are built.
---
+++ simulation/build/control_gui/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hayden/Capstone/SoftwareDevelopment/ROS2/simulation/src/control_gui
+# Build directory: /home/hayden/Capstone/SoftwareDevelopment/ROS2/simulation/build/control_gui
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
