a0e615ea0de76e63d4c75af670b2e25818487063
Andrew Burroughs <andrewburroughs17@gmail.com>
2026-05-17 00:17:44 -0500

    Added motors

--- shovel/launch/launch_talos_motors.py
+++ shovel/launch/launch_talos_motors.py
@@ -32,7 +32,8 @@ def generate_launch_description():
                 {"publishing_delay": 15},
                 {"kill_key": 50},
                 {"op_mode": 0},
-                {"print_data": True},
+                {"print_data": False},
+                {"can_interface": "can0"},
                 {"stop_topic": "talon_14_stop"}
             ],
             output={'stderr': 'screen', 'stdout': 'screen'},
@@ -57,9 +58,142 @@ def generate_launch_description():
                 {"publishing_delay": 15},
                 {"kill_key": 52},
                 {"op_mode": 0},
-                {"print_data": True},
+                {"print_data": False},
                 {"can_interface": "can0"},
                 {"stop_topic": "talon_16_stop"}
+            ],
+            output={'stderr': 'screen', 'stdout': 'screen'},
+            respawn=True
+        )
+        ,
+        Node(
+            package='talon',
+            name='Talon18',
+            executable='talon_node',
+            parameters=[
+                {"motor_number": 18},
+                {"diagnostics_port": 56714},
+                {"invert_motor": False},
+                {"speed_topic": "vibes_speed"},
+                {"info_topic": "talon_18_info"},
+                {"kP": 10.0},
+                {"kI": 0.000001},
+                {"kD": 0.000001},
+                {"kF": 0.0},
+                {"publishing_delay": 15},
+                {"kill_key": 52},
+                {"op_mode": 0},
+                {"print_data": False},
+                {"can_interface": "can0"},
+                {"stop_topic": "talon_16_stop"}
+            ],
+            output={'stderr': 'screen', 'stdout': 'screen'},
+            respawn=True
+        )
+        ,
+        Node(
+            package='kraken',
+            name='Kraken10',
+            executable='kraken_node',
+            parameters=[
+                {"motor_number": 10},
+                {"diagnostics_port": 72340},
+                {"invert_motor": False},
+                {"speed_topic": "falcon_10_speed"},
+                {"user_topic": "falcon_10_user_speed"},
+                {"reset_topic": "1"},
+                {"info_topic": "talon_10_info"},
+                {"kP": 10.0},
+                {"kI": 0.000001},
+                {"kD": 0.000001},
+                {"kF": 0.0},
+                {"publishing_delay": 15},
+                {"kill_key": 54},
+                {"op_mode": 0},
+                {"print_data": False},
+                {"can_interface": "can0"},
+                {"stop_topic": "falcon_10_stop"}
+            ],
+            output={'stderr': 'screen', 'stdout': 'screen'},
+            respawn=True
+        )
+	    ,
+        Node(
+            package='kraken',
+            name='Kraken11',
+            executable='kraken_node',
+            parameters=[
+                {"motor_number": 11},
+                {"diagnostics_port": 72341},
+                {"invert_motor": True},
+                {"speed_topic": "falcon_11_speed"},
+                {"user_topic": "falcon_11_user_speed"},
+                {"reset_topic": "2"},
+                {"info_topic": "talon_11_info"},
+                {"kP": 10.0},
+                {"kI": 0.000001},
+                {"kD": 0.000001},
+                {"kF": 0.0},
+                {"publishing_delay": 15},
+                {"kill_key": 55},
+                {"op_mode": 0},
+                {"print_data": False},
+                {"can_interface": "can0"},
+                {"stop_topic": "falcon_11_stop"}
+            ],
+            output={'stderr': 'screen', 'stdout': 'screen'},
+            respawn=True
+        )
+        ,
+        Node(
+            package='kraken',
+            name='Kraken12',
+            executable='kraken_node',
+            parameters=[
+                {"motor_number": 12},
+                {"diagnostics_port": 72342},
+                {"invert_motor": False},
+                {"speed_topic": "falcon_12_speed"},
+                {"user_topic": "falcon_12_user_speed"},
+                {"reset_topic": "3"},
+                {"info_topic": "talon_12_info"},
+                {"kP": 10.0},
+                {"kI": 0.000001},
+                {"kD": 0.000001},
+                {"kF": 0.0},
+                {"publishing_delay": 15},
+                {"kill_key": 56},
+                {"op_mode": 0},
+                {"print_data": False},
+                {"can_interface": "can0"},
+                {"stop_topic": "falcon_12_stop"}
+            ],
+            output={'stderr': 'screen', 'stdout': 'screen'},
+            respawn=True
+        )
+	    ,
+        Node(
+            package='kraken',
+            name='Kraken13',
+            executable='kraken_node',
+            parameters=[
+                {"motor_number": 13},
+                {"diagnostics_port": 72343},
+                {"invert_motor": True},
+                {"speed_topic": "falcon_13_speed"},
+                {"user_topic": "falcon_13_user_speed"},
+                {"reset_topic": "4"},
+                {"info_topic": "talon_13_info"},
+                {"kP": 10.0},
+                {"kI": 0.000001},
+                {"kD": 0.000001},
+                {"kF": 0.0},
+                {"publishing_delay": 15},
+                {"kill_key": 57},
+                {"op_mode": 0},
+                {"print_data": False},
+                {"can_interface": "can0"},
+                {"stop_topic": "falcon_13_stop"}
             ],
             output={'stderr': 'screen', 'stdout': 'screen'},
             respawn=True
